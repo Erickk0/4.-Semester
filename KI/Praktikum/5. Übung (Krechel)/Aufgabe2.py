5061b8da64fbf07f747e565f43d0cbb5fe020efb
Erickk0 <erick.zeiler@student.hs-rm.de>
2024-07-11 17:52:28 +0200

    .

--- KI/Praktikum/5. Übung (Krechel)/Aufgabe2.py
+++ KI/Praktikum/5. Übung (Krechel)/Aufgabe2.py
@@ -1,11 +1,38 @@
 import heapq
 
-from Aufgabe1 import Node, initial_board
+class Node:
+    def __init__(self, board, empty_tile_pos, moves=0, parent=None):
+        self.board = board
+        self.empty_tile_pos = empty_tile_pos
+        self.moves = moves
+        self.parent = parent
 
+    def __str__(self):
+        return str(self.board)
+
+    def get_neighbors(self):
+        neighbors = []
+        x, y = self.empty_tile_pos
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < 3 and 0 <= ny < 3:
+                new_board = [row[:] for row in self.board]
+                new_board[x][y], new_board[nx][ny] = new_board[nx][ny], new_board[x][y]
+                neighbors.append(Node(new_board, (nx, ny), self.moves + 1, self))
+        return neighbors
+
+    def is_goal(self):
+        return self.board == [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
+
+    def __lt__(self, other):
+        return self.moves < other.moves
+
+initial_board = [[2, 8, 3], [1, 6, 4], [7, 0, 5]]
 
 class PuzzleStateAStar(Node):
-    def __init__(self, board, empty_tile_pos, moves=0, heuristic_cost=0):
-        super().__init__(board, empty_tile_pos, moves)
+    def __init__(self, board, empty_tile_pos, moves=0, heuristic_cost=0, parent=None):
+        super().__init__(board, empty_tile_pos, moves, parent)
         self.heuristic_cost = heuristic_cost
 
     def __lt__(self, other):
@@ -26,6 +53,18 @@ def manhattan_distance(board):
                 distance += abs(goal_x - i) + abs(goal_y - j)
     return distance
 
+def print_solution(state):
+    path = []
+    while state:
+        path.append(state)
+        state = state.parent
+    path.reverse()
+    for step in path:
+        print(f'Move {step.moves}:')
+        for row in step.board:
+            print(row)
+        print()
+
 def a_star_search(initial_state):
     priority_queue = []
     heapq.heappush(priority_queue, initial_state)
@@ -35,6 +74,7 @@ def a_star_search(initial_state):
     while priority_queue:
         current_state = heapq.heappop(priority_queue)
         if current_state.is_goal():
+            print_solution(current_state)
             return current_state.moves
 
         for neighbor in current_state.get_neighbors():
